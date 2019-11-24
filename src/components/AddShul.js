443599b4a59350bcc3173e471d62863bc1dd143a
Ari <ari.abramowitz1@gmail.com>
2019-11-24 16:42:51 -0500

    added Israel cities to dropdown

--- src/components/AddShul.js
+++ src/components/AddShul.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import { withLocalize } from "react-localize-redux";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
-import { FormControl, InputLabel, Select, MenuItem, Paper, Typography} from '@material-ui/core';
+import { FormControl, InputLabel, Select, MenuItem, Paper, Typography, Divider} from '@material-ui/core';
+import * as israelCities from '../data/israel-cities';
 
-const ccs = require('countrycitystatejson')
+const csc = require('countrycitystatejson')
 
 
 const styles = theme => ({
@@ -26,43 +27,56 @@ class AddShul extends Component {
       this.state = {
         countries: [],
         selCountry: "",
-        states: [],
-        selState: "",
+        regions: [],
+        selRegion: "",
         cities: [],
         selCity: "",
       };
-  }
+
+    }
 
   handleCountrySelect(e){
     var countryCode = e.target.value;
-    var states = ccs.getStatesByShort(countryCode).sort();
-    if(states.length === 0){
-      states = ["N/A"];
+    var regions = [];
+    var adjustedCountryCode = countryCode;
+    if(countryCode === "IL-HE"){
+      adjustedCountryCode = "IL";
+    }
+    regions = this.getRegions(adjustedCountryCode);
+    if(regions.length === 0){
+      regions = [{
+        "en": "N/A",
+        "he": "N/A"
+      }];
       this.setState({
         selCountry: countryCode,
-        states: states,
+        regions: regions,
       }, () => {
         var e = {target: { value: "N/A"}};
-        this.handleStateSelect(e);
+        this.handleRegionSelect(e);
       })
     } else {
       this.setState({
         selCountry: countryCode,
-        states: states,
-        selState: "",
+        regions: regions,
+        selRegion: "",
         cities: [],
         selCity: "",
       })
     }
   }
 
-  handleStateSelect(e){
-    var state = e.target.value;
-    var cities = ccs.getCities(this.state.selCountry, state).sort();
+  handleRegionSelect(e){
+    var region = e.target.value;
+    var countryCode = this.state.selCountry;
+    if(countryCode === "IL-HE"){
+      countryCode = "IL";
+    }
+    var cities = this.getCities(countryCode, region);
     if(cities.length === 0){
       cities = ["N/A"];
       this.setState({
-        selState: state,
+        selRegion: region,
         cities: cities,
       }, () => {
         var e = {target: { value: "N/A"}};
@@ -70,7 +84,7 @@ class AddShul extends Component {
       })
     } else {
       this.setState({
-        selState: state,
+        selRegion: region,
         cities: cities,
       })
     }
@@ -82,12 +96,106 @@ class AddShul extends Component {
       selCity: city,
     })
   }
+
+  getRegions(countryCode){
+    if(countryCode === "IL") {
+      return this.getIsraelRegions();
+    } else {
+      return this.getGeneralRegions(countryCode);
+    }
+  }
+
+  getGeneralRegions(countryCode){ // reformatting general regions to match Israel regions
+    var regions = csc.getStatesByShort(countryCode).sort();
+    var formattedRegions = [];
+    for (let i = 0; i < regions.length; i++){
+      var regionName = regions[i]
+      var regionObj = {
+        "en": regionName,
+        "he": regionName
+      }
+      formattedRegions.push(regionObj)
+    }
+    return formattedRegions;
+  }
+
+  getIsraelRegions(){
+    var cities = israelCities.default;
+    var lishkaot = [];
+    for(let i = 0; i < cities.length; i++){
+      var currCity = cities[i];
+      var currLishka = {
+        "en": currCity["lishka_en"],
+        "he": currCity["lishka_he"]
+      };
+      const lishkaIndex = lishkaot.map(lishka => lishka["en"]).indexOf(currCity["lishka_en"]);
+      if(lishkaIndex === -1){
+        lishkaot.push(currLishka);
+      }
+    }
+    return lishkaot;
+  }
+
+  getCities(countryCode, region){
+    var cities = [];
+    if(countryCode === "IL"){
+      cities = this.getIsraelCities(region);
+    } else {
+      cities = this.getGeneralCities(countryCode, region);
+    }
+    return cities;
+  }
+
+  getIsraelCities(region){ // reformatting general cities to match Israel cities
+    var allCities = israelCities.default;
+    var regionCities = [];
+    for(let i = 0; i < allCities.length; i++){
+      var currCity = allCities[i];
+      var cityRegion = currCity["lishka_en"]
+      if(cityRegion === region){
+        var cityData = {
+          "en": currCity["english_name"],
+          "he": currCity["name"]
+        };
+        regionCities.push(cityData);
+      }
+    }
+    return regionCities;
+  }
+
+  getGeneralCities(countryCode, region){
+    var cities = csc.getCities(countryCode, region);
+    var formattedCities = [];
+    for (let i = 0; i < cities.length; i++){
+      var cityName = cities[i]
+      var cityObj = {
+        "en": cityName,
+        "he": cityName
+      }
+      formattedCities.push(cityObj)
+    }
+    return formattedCities;
+  }
+
   componentDidMount(){
-    var countries = ccs.getCountries();
+    this.getIsraelRegions();
+    var countries = csc.getCountries();
     var sortedCountries = countries.sort((a, b) => a.name.localeCompare(b.name));
     this.setState({
       countries: sortedCountries
     })
+  }
+
+  componentDidUpdate(){
+    if(this.state.selCountry === "IL" && this.props.activeLanguage && this.props.activeLanguage.code === "he"){
+      this.setState({
+        selCountry: "IL-HE"
+      })
+    } else if (this.state.selCountry === "IL-HE" && this.props.activeLanguage && this.props.activeLanguage.code === "en"){
+      this.setState({
+        selCountry: "IL"
+      })
+    }
   }
 
   render() {
@@ -95,34 +203,48 @@ class AddShul extends Component {
 
     const addShul = this.props.translate("addShul");
     const country = this.props.translate("country");
-    const stateRegion = this.props.translate("stateRegion");
+    const stateOrRegion = this.props.translate("stateOrRegion");
     const city = this.props.translate("city");
     const selectCity = this.props.translate("selectCity");
+
+    const isHebrew = (this.props.activeLanguage && this.props.activeLanguage.code === "he");
 
     var directionStyling = {
       direction: 'ltr',
       textAlign: 'left'
     }
+    var sortedRegions = this.state.regions.sort((a, b) => a["en"].localeCompare(b["en"]));
+    var sortedCities = this.state.cities.sort((a, b) => a["en"].localeCompare(b["en"]));
 
-    if (this.props.activeLanguage && this.props.activeLanguage.code === "he") {
+    if (isHebrew) {
       directionStyling = {
         direction: 'rtl',
         textAlign: 'right'
       }
-
+      sortedRegions = this.state.regions.sort((a, b) => a["he"].localeCompare(b["he"]));
+      sortedCities = this.state.cities.sort((a, b) => a["he"].localeCompare(b["en"]));
     }
 
     const countries = this.state.countries.map((country) => {
       return <MenuItem value={country.shortName}>{country.name}</MenuItem>
     });
-    const states = this.state.states.map((state) => {
-      return <MenuItem value={state}>{state}</MenuItem>
+    if(isHebrew){
+      countries.unshift(
+        <Divider />
+      )
+      countries.unshift(
+        <MenuItem value={"IL-HE"} style={{direction: directionStyling.direction}}>ישראל</MenuItem>
+      )
+    }
+    const isIsrael = ["IL-HE", "IL"].includes(this.state.selCountry)
+    const regions = sortedRegions.map((region) => {
+      return <MenuItem value={region["en"]} style={{direction: isIsrael ? directionStyling.direction : 'ltr'}}>{isHebrew ? region["he"] : region["en"]}</MenuItem>
     });
-    const cities = this.state.cities.map((city) => {
-      return <MenuItem value={city}>{city}</MenuItem>
+    const cities = sortedCities.map((city) => {
+      return <MenuItem value={city["he"]} style={{direction: isIsrael ? directionStyling.direction : 'ltr'}}>{isHebrew ? city["he"] : city["en"]}</MenuItem>
     });
-    const statesDisabled = this.state.selCountry === "";
-    const citiesDisabled = this.state.selState === "";
+    const regionsDisabled = this.state.selCountry === "";
+    const citiesDisabled = this.state.selRegion === "";
 
     return (
       <div>
@@ -146,15 +268,15 @@ class AddShul extends Component {
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel id="add-shul-state-label">{stateRegion}</InputLabel>
+            <InputLabel id="add-shul-region-label">{stateOrRegion}</InputLabel>
             <Select
-              labelId="add-shul-state-label"
-              id="add-shul-state-select"
-              value={this.state.selState}
-              onChange={(e) => {this.handleStateSelect(e)}}
-              disabled={statesDisabled}
+              labelId="add-shul-region-label"
+              id="add-shul-region-select"
+              value={this.state.selRegion}
+              onChange={(e) => {this.handleRegionSelect(e)}}
+              disabled={regionsDisabled}
             >
-              {states}
+              {regions}
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
