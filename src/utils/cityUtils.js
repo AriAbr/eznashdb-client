5fa03340103fab88eaa466c038fbc101a8846441
Ari Abramowitz <ari.abramowitz1@gmail.com>
2021-12-12 11:10:45 +0200

    More triple = fixing

--- src/utils/cityUtils.js
+++ src/utils/cityUtils.js
@@ -3,12 +3,12 @@ import * as israelCities from '../data/israel-cities';
 export const translateCityName = (cityName, toLang) => {
     try {
         const cities = israelCities.default;
-        if (toLang == "en") {
+        if (toLang === "en") {
             const city = cities.find(city => {
                 return city.name === cityName;
             })
             return city.english_name
-        } else if (toLang == "he") {
+        } else if (toLang === "he") {
             const city = cities.find(city => {
                 return city.english_name === cityName.toUpperCase() ;
             })
